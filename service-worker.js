3b3a1273ba5bd25dcf1d276de6eca860457d1805
syedalif2017-jpg <syedalif2017@gmail.com>
2026-04-30 10:16:06 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
-const CACHE = "app-v1";
+const CACHE_NAME = 'syed-app-v1';
+const assets = [
+  '/',
+  'index.html',
+  'admin.html',
+  'manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll([
-      "index.html",
-      "admin.html"
-    ]))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
